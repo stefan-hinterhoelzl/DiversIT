2282fda5c59d40c532d4836a49776e68c4c2308d
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-10-14 23:12:26 +0200

    added firebase

--- mintistcool/src/app/app.module.ts
+++ mintistcool/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as firebase from "./../../node_modules/firebase/app";
+import { environment } from 'src/environments/environment';
+
+const app = firebase.initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [
--- mintistcool/src/environments/environment.prod.ts
+++ mintistcool/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCgeYXPqIXsB27T5WMh4k7ruLExwanjZlg",
+    authDomain: "mintistcool-be74c.firebaseapp.com",
+    projectId: "mintistcool-be74c",
+    storageBucket: "mintistcool-be74c.appspot.com",
+    messagingSenderId: "169635813377",
+    appId: "1:169635813377:web:a44b782f659388f4e18800",
+    measurementId: "G-930B0KR1BL"
+  },
 };
