c8694c02c74c625c5893bca702287bb69664f00f
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-10-14 23:23:22 +0200

    firebase added, hosting started

--- mintistcool/src/environments/environment.ts
+++ mintistcool/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCgeYXPqIXsB27T5WMh4k7ruLExwanjZlg",
+    authDomain: "mintistcool-be74c.firebaseapp.com",
+    projectId: "mintistcool-be74c",
+    storageBucket: "mintistcool-be74c.appspot.com",
+    messagingSenderId: "169635813377",
+    appId: "1:169635813377:web:a44b782f659388f4e18800",
+    measurementId: "G-930B0KR1BL"
+  },
 };
 
 /*
