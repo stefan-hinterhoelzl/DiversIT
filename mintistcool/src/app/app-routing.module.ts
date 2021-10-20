880e0028419eca965da383246cedca63d6c9505d
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-10-20 22:22:10 +0200

    landing page, login box, main component

--- mintistcool/src/app/app-routing.module.ts
+++ mintistcool/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', pathMatch: 'full', redirectTo: 'app'},
+
+  {path: 'landing',
+  component: LandingPageComponent
+  },
+  {path: 'app',
+  component: MainComponent
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
--- mintistcool/src/app/app.module.ts
+++ mintistcool/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material-module';
 import { initializeApp } from '@firebase/app';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginBoxComponent } from './login-box/login-box.component';
+import { MainComponent } from './main/main.component';
 
 const app = initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginBoxComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
