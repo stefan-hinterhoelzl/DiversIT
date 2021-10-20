738683dbd6d6aaccd229bc4d41c579708697a171
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-10-20 22:54:18 +0200

    login page started

---
+++ mintistcool/src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {} from 'firebase/app'
+import { from } from "rxjs";
+
+
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthService
+ {
+ 
+
+
+
+
+
+
+
+
+}
+
