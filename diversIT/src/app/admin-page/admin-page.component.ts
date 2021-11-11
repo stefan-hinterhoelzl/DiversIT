1ceccf099b5499d03c5f39e51b31e4a62c9be4c1
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-11-11 15:33:03 +0100

    admin page tables, add relationship between mentor/mentee

--- diversIT/src/app/admin-page/admin-page.component.ts
+++ diversIT/src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { DiversITUser } from '../models/users.model';
+import { FirestoreService } from '../services/firestore.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
+  users: DiversITUser[]
+  dataSourceMentees: any
+  dataSourceMentors: any
+  dataSourceAdmins: any
+  displayedColumnsAdmins: string[] = ['email', 'lastLogin', 'creationDate'];
+  displayedColumns: string[] = ['email', 'lastLogin', 'creationDate'];
 
-  ngOnInit(): void {
+  constructor(private firestore: FirestoreService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.users = await this.firestore.getAllUsersPromise();
+
+    this.dataSourceMentees = new MatTableDataSource(this.users.filter((element) => element.role == 3))
+    this.dataSourceMentors = new MatTableDataSource(this.users.filter((element) => element.role == 2))
+    this.dataSourceAdmins = new MatTableDataSource(this.users.filter((element) => element.role == 1))
   }
+
+  applyFilterMentee(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceMentees.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyFilterMentors(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceMentees.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyFilterAdmins(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceMentees.filter = filterValue.trim().toLowerCase();
+  }
+
+
 
 }
