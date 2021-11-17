673fcb5747b64352d32ad4e097501de63e67336f
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-11-17 23:51:03 +0100

    hotfix again

--- diversIT/src/app/chat/chat.component.ts
+++ diversIT/src/app/chat/chat.component.ts
@@ -56,7 +56,7 @@ export class ChatComponent implements OnInit {
     this.messageSubscription = this.firestore.messagesStatus.subscribe((data) => {
       this.messages = data;
       console.log(data);
-      if (this.messages != null) this.messages.sort((a,b) => b.timestamp.toMillis() - a.timestamp.toMillis())
+      if (this.messages != null) this.messages.sort((a,b) => a.timestamp.toMillis() - b.timestamp.toMillis())
     });
     this.chatOpen = true;
     this.activeChat = chat.uid;
