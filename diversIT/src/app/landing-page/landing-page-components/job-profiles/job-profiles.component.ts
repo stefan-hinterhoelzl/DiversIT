4e4ea1967cefc4fb286889218a7825c6e065cd50
coeys <76596815+coeys@users.noreply.github.com>
2021-12-03 21:41:34 +0100

    CR for job profile mentors from db

--- diversIT/src/app/landing-page/landing-page-components/job-profiles/job-profiles.component.ts
+++ diversIT/src/app/landing-page/landing-page-components/job-profiles/job-profiles.component.ts
@@ -1,5 +1,7 @@
+import { UserService } from './../../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { DiversITUser } from 'src/app/models/users.model';
 
 @Component({
   selector: 'app-job-profiles',
@@ -9,15 +11,25 @@ import { FormControl } from '@angular/forms';
 export class JobProfilesComponent implements OnInit {
 
   breakpoint: number;
-  jobProfiles = new FormControl();
+  numberOfMentors = 3;
+  selectedItems: string[] = ['DevOps Engineer', 'Universitätsprofessor für Informatik', 'Product Owner', 'IT-Systemadministrator'];
+  jobProfiles = new FormControl(this.selectedItems);
   jobProfilesList: string[] = ['IT-Systemadministrator', 'IT-Techniker', 'UX-Designer', 'Software Engineer', 'Universitätsprofessor für Informatik', 'Scrum Master', 'DevOps Engineer', 'Product Owner'].sort();
-  selectedItems: string[] = ['DevOps Engineer', 'IT-Systemadministrator', 'Product Owner', 'UX-Designer'];
+  mentors: DiversITUser[];
+  jobProfilesMentorsMap: Map<string, DiversITUser[]>;
 
+  constructor(private userService: UserService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.breakpoint = (window.innerWidth <= 1220) ? 1 : 2;
+    this.initializeJobProfilesMentorsMap(this.jobProfilesList);
+
+    await this.userService.getAllMentorsPromise().then(data => {
+      this.mentors = data;
+    });
+
+    this.setJobProfilesMentors(this.jobProfilesList, this.mentors);
+
   }
 
   onResize(event) {
@@ -39,6 +51,31 @@ export class JobProfilesComponent implements OnInit {
     } else {
       this.selectedItems.push(value);
     }
+  }
+
+  initializeJobProfilesMentorsMap(jobProfiles: string[]) {
+    this.jobProfilesMentorsMap = new Map<string, DiversITUser[]>();
+    jobProfiles.forEach(job => {
+      this.jobProfilesMentorsMap.set(job, []);
+    });
+  }
+
+  setJobProfilesMentors(jobProfiles: string[], mentors: DiversITUser[]): void {
+    jobProfiles.forEach(job => {
+      let jobMentors = mentors.filter(m => m.job === job);
+      if (jobMentors.length > 1) {
+        jobMentors = jobMentors.sort(() => .5 - Math.random()).slice(0, this.numberOfMentors);
+      }
+      this.jobProfilesMentorsMap.set(job, jobMentors);
+    });
+  }
+
+  firstMentorHasImage(jobProfile: string): boolean {
+    return this.jobProfilesMentorsMap
+      && this.jobProfilesMentorsMap.get(jobProfile)
+      && this.jobProfilesMentorsMap.get(jobProfile).length > 0
+      && this.jobProfilesMentorsMap.get(jobProfile)[0].photoURL !== ''
+      && this.jobProfilesMentorsMap.get(jobProfile)[0].photoURL !== 'unknown';
   }
 
 }
