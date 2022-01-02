a812ef27d8c4db7d9b414bd17b8bb4136768edba
deutho <t_deutsch@gmx.at>
2022-01-02 16:40:56 +0100

    Testing: First Components changed to not throw errors

--- diversIT/src/app/profile-page/profile-page.component.spec.ts
+++ diversIT/src/app/profile-page/profile-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackbarComponent } from '../snackbar/snackbar.component';
 
 import { ProfilePageComponent } from './profile-page.component';
 
@@ -8,7 +10,10 @@ describe('ProfilePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfilePageComponent ]
+      providers: [
+        ProfilePageComponent,
+        { provide: [SnackbarComponent, MatSnackBar]}
+      ]
     })
     .compileComponents();
   });
