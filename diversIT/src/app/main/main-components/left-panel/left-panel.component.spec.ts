b804a76cdb88fac706562c3f8d5773bd433ca5b9
deutho <t_deutsch@gmx.at>
2022-01-03 13:51:34 +0100

    Test: left-panel compiles now

--- diversIT/src/app/main/main-components/left-panel/left-panel.component.spec.ts
+++ diversIT/src/app/main/main-components/left-panel/left-panel.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { SnackbarComponent } from 'src/app/snackbar/snackbar.component';
+import { environment } from 'src/environments/environment';
 
 import { LeftPanelComponent } from './left-panel.component';
 
@@ -7,8 +11,13 @@ describe('LeftPanelComponent', () => {
   let fixture: ComponentFixture<LeftPanelComponent>;
 
   beforeEach(async () => {
+    let app = initializeApp(environment.firebaseConfig);
     await TestBed.configureTestingModule({
-      declarations: [ LeftPanelComponent ]
+      declarations: [ LeftPanelComponent ],
+      providers: [
+        {provide: SnackbarComponent, useValue: {}},
+        {provide: Router, useValue:{}}
+      ]
     })
     .compileComponents();
   });
