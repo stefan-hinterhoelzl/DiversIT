fac0e9a9507eacca3cd3bbdcd2c518dc205e4d7c
deutho <t_deutsch@gmx.at>
2022-01-03 13:11:09 +0100

    Test: message card no errors anymore

--- diversIT/src/app/forum-thread/message-card/message-card.component.spec.ts
+++ diversIT/src/app/forum-thread/message-card/message-card.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { SnackbarComponent } from 'src/app/snackbar/snackbar.component';
+import { environment } from 'src/environments/environment';
 
 import { MessageCardComponent } from './message-card.component';
 
@@ -6,9 +11,28 @@ describe('MessageCardComponent', () => {
   let component: MessageCardComponent;
   let fixture: ComponentFixture<MessageCardComponent>;
 
-  beforeEach(async () => {
+  class MockMatDialog{
+
+  }
+
+  let routeData = {
+    snapshot: {
+      paramMap: convertToParamMap({
+        id: "QwOPRzzeBxBS3wrw3LwC",
+      })
+    }}
+  
+
+  beforeEach(async () => {    
+    let app = initializeApp(environment.firebaseConfig);
     await TestBed.configureTestingModule({
-      declarations: [ MessageCardComponent ]
+      declarations: [ MessageCardComponent ],
+      providers: [
+        {provide: MatDialog, useClass: MockMatDialog},
+        {provide: ActivatedRoute, useValue: routeData},
+        {provide: Router, useValue: {}},
+        {provide: SnackbarComponent, useValue:{}}
+      ]
     })
     .compileComponents();
   });
