5f2cc0b5bf74c664de88dc9edc77f979539cfe2f
deutho <t_deutsch@gmx.at>
2022-01-03 11:02:00 +0100

    Tests: admin-page does not throw errors any more

--- diversIT/src/app/admin-page/admin-page.component.spec.ts
+++ diversIT/src/app/admin-page/admin-page.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { environment } from 'src/environments/environment';
+import { SnackbarComponent } from '../snackbar/snackbar.component';
 
 import { AdminPageComponent } from './admin-page.component';
 
@@ -6,20 +10,26 @@ describe('AdminPageComponent', () => {
   let component: AdminPageComponent;
   let fixture: ComponentFixture<AdminPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdminPageComponent ]
-    })
-    .compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  class MockSnackbar{
+
+  }
+  class MockRouter{
+
+  }
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      providers: [ AdminPageComponent,
+        {provide: SnackbarComponent, useClass: MockSnackbar},
+        {provide: Router, useClass: MockRouter},]
+    });    
+    let app = initializeApp(environment.firebaseConfig);
+    component = TestBed.inject(AdminPageComponent)
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
