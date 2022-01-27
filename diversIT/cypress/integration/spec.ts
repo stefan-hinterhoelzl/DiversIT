4410282e5e604c66273cf7277b7a81aafe4533d0
coeys <76596815+coeys@users.noreply.github.com>
2022-01-27 09:13:48 +0100

    cypress tests: added 3 more authguard smoke tests

--- diversIT/cypress/integration/spec.ts
+++ diversIT/cypress/integration/spec.ts
@@ -1,6 +1,36 @@
 describe('Smoke test', () => {
-  it('Visits to the root page redirect to landing page if not authenticated', () => {
+  it('redirects to landing page on unauthenticated access to root page', () => {
     cy.visit('/');
+    cy.url().should('include', '/landing');
+    cy.get('app-navbar a').contains('DiversIT');
+
+    const snackbar = cy.get('simple-snack-bar');
+    snackbar.should('be.visible');
+    snackbar.should('have.text', 'Sie sind nicht eingeloggt! ');
+  });
+
+  it('redirects to landing page on unauthenticated access to app page', () => {
+    cy.visit('/app');
+    cy.url().should('include', '/landing');
+    cy.get('app-navbar a').contains('DiversIT');
+
+    const snackbar = cy.get('simple-snack-bar');
+    snackbar.should('be.visible');
+    snackbar.should('have.text', 'Sie sind nicht eingeloggt! ');
+  });
+
+  it('redirects to landing page on unauthenticated access to chat page', () => {
+    cy.visit('/chat');
+    cy.url().should('include', '/landing');
+    cy.get('app-navbar a').contains('DiversIT');
+
+    const snackbar = cy.get('simple-snack-bar');
+    snackbar.should('be.visible');
+    snackbar.should('have.text', 'Sie sind nicht eingeloggt! ');
+  });
+
+  it('redirects to landing page on unauthenticated access to a profile page', () => {
+    cy.visit('/profile/zLccCJEzSGap7BlhefDT0xTyi9I3');
     cy.url().should('include', '/landing');
     cy.get('app-navbar a').contains('DiversIT');
 
