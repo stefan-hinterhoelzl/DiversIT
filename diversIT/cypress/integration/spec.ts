283d98252d62d18618423bed61c31a3e8df4321e
coeys <76596815+coeys@users.noreply.github.com>
2022-01-27 08:56:13 +0100

    cypress tests: fix smoke test

--- diversIT/cypress/integration/spec.ts
+++ diversIT/cypress/integration/spec.ts
@@ -1,7 +1,11 @@
 describe('Smoke test', () => {
   it('Visits to the root page redirect to landing page if not authenticated', () => {
-    cy.visit('/')
-    cy.url().should('include', '/landing')
-    cy.get('app-root .mat-toolbar span').contains('DiversIT')
-  })
+    cy.visit('/');
+    cy.url().should('include', '/landing');
+    cy.get('app-navbar a').contains('DiversIT');
+
+    const snackbar = cy.get('simple-snack-bar');
+    snackbar.should('be.visible');
+    snackbar.should('have.text', 'Sie sind nicht eingeloggt! ');
+  });
 })
